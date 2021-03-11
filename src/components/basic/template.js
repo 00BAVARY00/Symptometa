a8def141f952e23fa34797bb12d9e5c11b6730aa
Mahmoud Mohamed Gomaa <64234882+Bavary00@users.noreply.github.com>
2021-03-11 18:02:29 +0200

    Update template.js
    phone

--- src/components/basic/template.js
+++ src/components/basic/template.js
@@ -46,7 +46,7 @@ const template = (context) => {
           </form>
           <p class="text-muted small" style="position: absolute; bottom:8px; left:16px; font-size: 18px;">
             <i class="fa fa-info-circle"></i>
-            The gender and age attributes are required.
+            Gender & Age are required.
             </p>
         </div>
       `);
